bf379456505d43b493b7c99ab01b867e5f21ea63
necropotame <necropotame@gmail.com>
2017-09-05 00:19:00 +0200

    Traduction terminée

--- questions.js
+++ questions.js
@@ -341,6 +341,7 @@ questions = [
     },
     {
         "question": "Government surveillance is necessary in the modern world.",
+        "question_fr": "La surveillance par le gouvernement est une necessité dans le monde moderne.",
         "effect": {
             "econ": 0,
             "dipl": 0,
@@ -350,6 +351,7 @@ questions = [
     },
     {
         "question": "The very existence of the state is a threat to our liberty.",
+        "question_fr": "L'existence même de l'État est une menace à nos libertés.",
         "effect": {
             "econ": 0,
             "dipl": 0,
@@ -359,6 +361,7 @@ questions = [
     },
     {
         "question": "Regardless of political opinions, it is important to side with your country.",
+        "question_fr": "Quelque soit la ligne politique, il est important de soutenir son pays.",
         "effect": {
             "econ": 0,
             "dipl": -10,
@@ -368,6 +371,7 @@ questions = [
     },
     {
         "question": "No authority should be left unquestioned.",
+        "question_fr": "Toutes les authorités devraient être remises en question.",
         "effect": {
             "econ": 0,
             "dipl": 0,
@@ -377,6 +381,7 @@ questions = [
     },
     {
         "question": "A hierarchical state is best.",
+        "question_fr": "Un État hierarchique est préférable.",
         "effect": {
             "econ": 0,
             "dipl": 0,
@@ -386,6 +391,7 @@ questions = [
     },
     {
         "question": "It is important that the government follows the majority opinion, even if it is wrong.",
+        "question_fr": "Il est important que le gouvernement suive l'opinion majoritaire, même si elle se trompe.",
         "effect": {
             "econ": 0,
             "dipl": 0,
@@ -395,6 +401,7 @@ questions = [
     },
     {
         "question": "The stronger the leadership, the better.",
+        "question_fr": "Plus fort est le leadership, meilleur cela est.",
         "effect": {
             "econ": 0,
             "dipl": -10,
@@ -404,6 +411,7 @@ questions = [
     },
     {
         "question": "Democracy is more than a decision-making process.",
+        "question_fr": "La démocratie est plus qu'un processus de prise de décisions.",
         "effect": {
             "econ": 0,
             "dipl": 0,
@@ -413,6 +421,7 @@ questions = [
     },
     {
         "question": "Environmental regulations are essential.",
+        "question_fr": "Les régulations environementales sont essentielles.",
         "effect": {
             "econ": 5,
             "dipl": 0,
@@ -422,6 +431,7 @@ questions = [
     },
     {
         "question": "A better world will come from automation, science, and technology.",
+        "question_fr": "Un monde meilleur viendra de l'automatisation, la science et la technologie.",
         "effect": {
             "econ": 0,
             "dipl": 0,
@@ -431,6 +441,7 @@ questions = [
     },
     {
         "question": "Children should be educated in religious or traditional values.",
+        "question_fr": "Les enfants devraient être éduqués avec les valeus religieuses ou traditionelles.",
         "effect": {
             "econ": 0,
             "dipl": 0,
@@ -440,6 +451,7 @@ questions = [
     },
     {
         "question": "Traditions are of no value on their own.",
+        "question_fr": "Les traditions n'ont aucune valeurs en elles-mêmes.",
         "effect": {
             "econ": 0,
             "dipl": 0,
@@ -449,6 +461,7 @@ questions = [
     },
     {
         "question": "Religion should play a role in government.",
+        "question_fr": "La religion devrait jouer un rôle dans le gouvernement.",
         "effect": {
             "econ": 0,
             "dipl": 0,
@@ -458,6 +471,7 @@ questions = [
     },
     {
         "question": "Churches should be taxed the same way other institutions are taxed.",
+        "question_fr": "Les églises devraient être taxées de la même façon que les autres institutions.",
         "effect": {
             "econ": 5,
             "dipl": 0,
@@ -467,6 +481,7 @@ questions = [
     },
     {
         "question": "Climate change is currently one of the greatest threats to our way of life.",
+        "question_fr": "Le réchauffement climatique est actuellement une des plus grandes menaces à notre façon de vivre.",
         "effect": {
             "econ": 0,
             "dipl": 0,
@@ -476,6 +491,7 @@ questions = [
     },
     {
         "question": "It is important that we work as a united world to combat climate change.",
+        "question_fr": "Il est important que le monde travail de façon unie face au rechauffement climatique.",
         "effect": {
             "econ": 0,
             "dipl": 10,
@@ -485,6 +501,7 @@ questions = [
     },
     {
         "question": "Society was better many years ago than it is now.",
+        "question_fr": "La société était meilleur avant.",
         "effect": {
             "econ": 0,
             "dipl": 0,
@@ -494,6 +511,7 @@ questions = [
     },
     {
         "question": "It is important that we maintain the traditions of our past.",
+        "question_fr": "Il est important de préserver nos traditions passées.",
         "effect": {
             "econ": 0,
             "dipl": 0,
@@ -503,6 +521,7 @@ questions = [
     },
     {
         "question": "It is important that we think in the long term, beyond our lifespans.",
+        "question_fr": "Il est important que nous réfléchissons sur le long terme, au délà même de notre propre espérance de vie.",
         "effect": {
             "econ": 0,
             "dipl": 0,
@@ -512,6 +531,7 @@ questions = [
     },
     {
         "question": "Reason is more important than maintaining our culture.",
+        "question_fr": "La Raison est plus importante que de préserver notre culture.",
         "effect": {
             "econ": 0,
             "dipl": 0,
@@ -521,6 +541,7 @@ questions = [
     },
     {
         "question": "Drug use should be legalized or decriminalized.",
+        "question_fr": "Les drogues devraient être légalisées ou décriminalisées.",
         "effect": {
             "econ": 0,
             "dipl": 0,
@@ -530,6 +551,7 @@ questions = [
     },
     {
         "question": "Same-sex marriage should be legal.",
+        "question_fr": "Le mariage homosexuel devrait être légal.",
         "effect": {
             "econ": 0,
             "dipl": 0,
@@ -539,6 +561,7 @@ questions = [
     },
     {
         "question": "No cultures are superior to others.",
+        "question_fr": "Aucune culture n'est supérieur à une autre.",
         "effect": {
             "econ": 0,
             "dipl": 10,
@@ -548,6 +571,7 @@ questions = [
     },
     {
         "question": "Sex outside marriage is immoral.",
+        "question_fr": "Le sexe en dehors du mariage est immorale.",
         "effect": {
             "econ": 0,
             "dipl": 0,
@@ -557,6 +581,7 @@ questions = [
     },
     {
         "question": "If we accept migrants at all, it is important that they assimilate into our culture.",
+        "question_fr": "Si l'on accepte des migrants, il est important qu'ils assimiles notre culture.",
         "effect": {
             "econ": 0,
             "dipl": 0,
@@ -566,6 +591,7 @@ questions = [
     },
     {
         "question": "Abortion should be prohibited in most or all cases.",
+        "question_fr": "L'avortement devrait être interdite dans la plus-part des cas.",
         "effect": {
             "econ": 0,
             "dipl": 0,
@@ -575,6 +601,7 @@ questions = [
     },
     {
         "question": "Gun ownership should be prohibited for those without a valid reason.",
+        "question_fr": "Le port d'arme devrait être interdit à ceux qui n'ont pas une raison valable de s'en servir.",
         "effect": {
             "econ": 0,
             "dipl": 10,
@@ -584,6 +611,7 @@ questions = [
     },
     {
         "question": "I support single-payer, universal healthcare.",
+        "question_fr": "Je supporte l'assurance maladie universelle.",
         "effect": {
             "econ": 10,
             "dipl": 0,
@@ -593,6 +621,7 @@ questions = [
     },
     {
         "question": "Prostitution should be illegal.",
+        "question_fr": "La prostitution devrait être illégale.",
         "effect": {
             "econ": 0,
             "dipl": 0,
@@ -602,6 +631,7 @@ questions = [
     },
     {
         "question": "Maintaining family values is essential.",
+        "question_fr": "Préserver les valeurs familiales est essentiel.",
         "effect": {
             "econ": 0,
             "dipl": 0,
@@ -611,6 +641,7 @@ questions = [
     },
     {
         "question": "To chase progress at all costs is dangerous.",
+        "question_fr": "Poursuivre le progrès à tout prix est dangereux.",
         "effect": {
             "econ": 0,
             "dipl": 0,
@@ -620,6 +651,7 @@ questions = [
     },
     {
         "question": "Genetic modification is a force for good, even on humans.",
+        "question_fr": "Les modifications génétiques sont de bonnes techniques, même appliquées aux humains.",
         "effect": {
             "econ": 0,
             "dipl": 0,
@@ -629,6 +661,7 @@ questions = [
     },
     {
         "question": "We should open our borders to immigration.",
+        "question_fr": "Nous devriont ouvrir nos frontières aux immigrants.",
         "effect": {
             "econ": 0,
             "dipl": 10,
@@ -638,6 +671,7 @@ questions = [
     },
     {
         "question": "Governments should be as concerned about foreign citizens as they are about those within their borders.",
+        "question_fr": "Les gouvernements devraient être autant concerné par les citoyens des autres pays que par ceux vivant dans leurs frontières.",
         "effect": {
             "econ": 10,
             "dipl": 10,
@@ -647,6 +681,7 @@ questions = [
     },
     {
         "question": "All people - regardless of factors like culture or sexuality - should be treated equally.",
+        "question_fr": "Toutes les personnnes, quelque soit les facteurs de culture ou de sexualité, devraient être traitées également.",
         "effect": {
             "econ": 10,
             "dipl": 10,
@@ -656,6 +691,7 @@ questions = [
     },
     {
         "question": "It is important that we further my group's goals above all others.",
+        "question_fr": "Il est important de nous favorisions les objectifs de mon groupe plutôt que ceux des autres.",
         "effect": {
             "econ": -10,
             "dipl": -10,
