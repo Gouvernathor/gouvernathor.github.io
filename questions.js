f0e127821329a7d739cd3516ecbb5456fa7a09f8
necropotame <necropotame@gmail.com>
2017-09-05 16:31:34 +0200

    Defaults values

--- questions.js
+++ questions.js
@@ -1,34 +1,46 @@
 questions = [
     {
-        "question": "Les moyens de production ne devraient pas être des propriétés privées.",
-        "effect": {
-            "cause": 0,
-            "economy": -10
-        },
-        "answer": 0
+        question: "Les moyens de production ne devraient pas être des propriétés privées.",
+        answer: 0,
+        cause: 0,
+        boundaries: 0,
+        economy: -10,
+        society: 0,
+        ecosystem: 0,
+        rights: 0,
+        strategy: 0
     },
     {
-        "question": "La recherche de son propre profit est saine pour l'économie.",
-        "effect": {
-            "cause": 0,
-            "economy": 10
-        },
-        "answer": 0
+        question: "La recherche de son propre profit est saine pour l'économie.",
+        answer: 0,
+        cause: 0,
+        boundaries: 0,
+        economy: 10,
+        society: 0,
+        ecosystem: 0,
+        rights: 0,
+        strategy: 0
     },
     {
-        "question": "C'est le mérite qui explique la difference de richesse entre deux personnes.",
-        "effect": {
-            "cause": 0,
-            "economy": 10
-        },
-        "answer": 0
+        question: "C'est le mérite qui explique la difference de richesse entre deux personnes.",
+        answer: 0,
+        cause: 0,
+        boundaries: 0,
+        economy: 10,
+        society: 0,
+        ecosystem: 0,
+        rights: 0,
+        strategy: 0
     },
     {
-        "question": "Les plus-values sont du vol",
-        "effect": {
-            "cause": 0,
-            "economy": -10
-        },
-        "answer": 0
+        question: "Les plus-values sont du vol",
+        answer: 0,
+        cause: 0,
+        boundaries: 0,
+        economy: -10,
+        society: 0,
+        ecosystem: 0,
+        rights: 0,
+        strategy: 0
     }
 ];
