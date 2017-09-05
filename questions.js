b4253ec635a81852721087e081eed4602cb11346
necropotame <necropotame@gmail.com>
2017-09-05 17:34:00 +0200

    Questions sur l'économie

--- questions.js
+++ questions.js
@@ -1,6 +1,6 @@
 questions = [
     {
-        question: "Les moyens de production ne devraient pas être des propriétés privées.",
+        question: "Les moyens de production ne devraient pas pouvoir être des propriétés privées.",
         answer: 0,
         cause: 0,
         boundaries: 0,
@@ -33,11 +33,121 @@ questions = [
         strategy: 0
     },
     {
-        question: "Les plus-values sont du vol",
+        question: "Une entreprise privée employant des salariés commet un acte comparable au vol.",
         answer: 0,
         cause: 0,
         boundaries: 0,
         economy: -10,
+        society: 0,
+        ecosystem: 0,
+        rights: 0,
+        strategy: 0
+    },
+    {
+        question: "C'est l'offre et la demande qui permet de définir le prix d'un bien.",
+        answer: 0,
+        cause: 0,
+        boundaries: 0,
+        economy: 10,
+        society: 0,
+        ecosystem: 0,
+        rights: 0,
+        strategy: 0
+    },
+    {
+        question: "Idéalement, la monnaie devrait être abolit.",
+        answer: 0,
+        cause: 0,
+        boundaries: 0,
+        economy: -10,
+        society: 0,
+        ecosystem: 0,
+        rights: 0,
+        strategy: 0
+    },
+    {
+        question: "Actuelement, un salarié seul est libre de ses choix lors de la signature de son contrat avec son future employeur.",
+        answer: 0,
+        cause: 0,
+        boundaries: 0,
+        economy: 10,
+        society: 0,
+        ecosystem: 0,
+        rights: 0,
+        strategy: 0
+    },
+    {
+        question: "Il est important que la santé reste majoritairement publique.",
+        answer: 0,
+        cause: 0,
+        boundaries: 0,
+        economy: -10,
+        society: 0,
+        ecosystem: 0,
+        rights: 0,
+        strategy: 0
+    },
+    {
+        question: "Les écoles et les universités peuvent être privée.",
+        answer: 0,
+        cause: 0,
+        boundaries: 0,
+        economy: 10,
+        society: 0,
+        ecosystem: 0,
+        rights: 0,
+        strategy: 0
+    },
+    {
+        question: "L'énergie et les infrastructures de transport doivent être publique.",
+        answer: 0,
+        cause: 0,
+        boundaries: 0,
+        economy: -10,
+        society: 0,
+        ecosystem: 0,
+        rights: 0,
+        strategy: 0
+    },
+    {
+        question: "Les délocalisations sont un mal necessaire pour améliorer la production.",
+        answer: 0,
+        cause: 0,
+        boundaries: 0,
+        economy: 10,
+        society: 0,
+        ecosystem: 0,
+        rights: 0,
+        strategy: 0
+    },
+    {
+        question: "Le marché du travail est necessaire.",
+        answer: 0,
+        cause: 0,
+        boundaries: 0,
+        economy: 10,
+        society: 0,
+        ecosystem: 0,
+        rights: 0,
+        strategy: 0
+    },
+    {
+        question: "Les décisions prisent par les actionnaires des entreprises privés ont des effets globalement négatifs sur la société.",
+        answer: 0,
+        cause: 0,
+        boundaries: 0,
+        economy: -10,
+        society: 0,
+        ecosystem: 0,
+        rights: 0,
+        strategy: 0
+    },
+    {
+        question: "Il est normal qu'il y ait des riches et des pauvres.",
+        answer: 0,
+        cause: 0,
+        boundaries: 0,
+        economy: 10,
         society: 0,
         ecosystem: 0,
         rights: 0,
