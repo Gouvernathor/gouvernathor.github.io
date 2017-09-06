59729996ea519c7a4c0b63b9d0f88a455cecad22
necropotame <necropotame@gmail.com>
2017-09-06 02:05:06 +0200

    Questions sur le nationalisme et l'essentialisme

--- questions.js
+++ questions.js
@@ -1,4 +1,264 @@
 questions = [
+    
+    /* ESSENTIALISME ************************************************ */
+    {
+        question: "La place des hommes et des femmes dans la société peut-être en parti expliqué par la biologie.",
+        answer: 0,
+        cause: 10,
+        boundaries: 0,
+        economy: 0,
+        society: 0,
+        ecosystem: 0,
+        rights: 0,
+        strategy: 0
+    },
+    {
+        question: "Certains traits de charactère sont feminins alors que d'autre sont masculins.",
+        answer: 0,
+        cause: 10,
+        boundaries: 0,
+        economy: 0,
+        society: 0,
+        ecosystem: 0,
+        rights: 0,
+        strategy: 0
+    },
+    {
+        question: "« On ne nait pas femme, on le devient ».",
+        answer: 0,
+        cause: -10,
+        boundaries: 0,
+        economy: 0,
+        society: 0,
+        ecosystem: 0,
+        rights: 0,
+        strategy: 0
+    },
+    {
+        question: "Les aggressions sexuelles sont en partie causées par les pulsions naturelles des hommes.",
+        answer: 0,
+        cause: 10,
+        boundaries: 0,
+        economy: 0,
+        society: 0,
+        ecosystem: 0,
+        rights: 0,
+        strategy: 0
+    },
+    {
+        question: "Les genres doivent être abolit.",
+        answer: 0,
+        cause: -10,
+        boundaries: 0,
+        economy: 0,
+        society: 0,
+        ecosystem: 0,
+        rights: 0,
+        strategy: 0
+    },
+    {
+        question: "Un enfant doit être éduqué par son père et sa mère biologique car c'est le modèle naturel.",
+        answer: 0,
+        cause: 10,
+        boundaries: 0,
+        economy: 0,
+        society: 0,
+        ecosystem: 0,
+        rights: 0,
+        strategy: 0
+    },
+    {
+        question: "Dans l'espèce humaine, l'homosexualité est autant naturelle que l'hétérosexualité.",
+        answer: 0,
+        cause: -10,
+        boundaries: 0,
+        economy: 0,
+        society: 0,
+        ecosystem: 0,
+        rights: 0,
+        strategy: 0
+    },
+    {
+        question: "La biologie permet d'expliquer les differences de comportement entre differents groupes éthniques.",
+        answer: 0,
+        cause: 10,
+        boundaries: 0,
+        economy: 0,
+        society: 0,
+        ecosystem: 0,
+        rights: 0,
+        strategy: 0
+    },
+    {
+        question: "Les personnes trans-genre ne seront jamais vraiment du genre qu'ils souhaitent immiter.",
+        answer: 0,
+        cause: 10,
+        boundaries: 0,
+        economy: 0,
+        society: 0,
+        ecosystem: 0,
+        rights: 0,
+        strategy: 0
+    },
+    {
+        question: "Certains groupes de personnes ont un fonctionnement qui leur est propre et il en sera toujours ainsi.",
+        answer: 0,
+        cause: 10,
+        boundaries: 0,
+        economy: 0,
+        society: 0,
+        ecosystem: 0,
+        rights: 0,
+        strategy: 0
+    },
+    {
+        question: "Les differences de traitements et de qualité de vie dans notre société montrent que le racisme est encore très présent.",
+        answer: 0,
+        cause: -10,
+        boundaries: 0,
+        economy: 0,
+        society: 0,
+        ecosystem: 0,
+        rights: 0,
+        strategy: 0
+    },
+    {
+        question: "Les sciences dites « dures » sont subjectives et reproduisent les structures sociales de notre société.",
+        answer: 0,
+        cause: -10,
+        boundaries: 0,
+        economy: 0,
+        society: 0,
+        ecosystem: 0,
+        rights: 0,
+        strategy: 0
+    },
+    {
+        question: "Les differences de traitements et de qualité de vie dans notre société montrent que le racisme est encore très présent.",
+        answer: 0,
+        cause: -10,
+        boundaries: 0,
+        economy: 0,
+        society: 0,
+        ecosystem: 0,
+        rights: 0,
+        strategy: 0
+    },
+    
+    /* NATIONALISME ************************************************* */
+    {
+        question: "Un peu de chauvinisme dans les compétitions sportives est accepable.",
+        answer: 0,
+        cause: 0,
+        boundaries: 10,
+        economy: 0,
+        society: 0,
+        ecosystem: 0,
+        rights: 0,
+        strategy: 0
+    },
+    {
+        question: "Les citoyens doivent être prioritaire par rapport aux étrangers.",
+        answer: 0,
+        cause: 0,
+        boundaries: 10,
+        economy: 0,
+        society: 0,
+        ecosystem: 0,
+        rights: 0,
+        strategy: 0
+    },
+    {
+        question: "Les valeurs de mon pays son supérieurs à celles des autres.",
+        answer: 0,
+        cause: 0,
+        boundaries: 10,
+        economy: 0,
+        society: 0,
+        ecosystem: 0,
+        rights: 0,
+        strategy: 0
+    },
+    {
+        question: "Le multi-culturalisme est un danger.",
+        answer: 0,
+        cause: 0,
+        boundaries: 10,
+        economy: 0,
+        society: 0,
+        ecosystem: 0,
+        rights: 0,
+        strategy: 0
+    },
+    {
+        question: "Un bon citoyen est patriote.",
+        answer: 0,
+        cause: 0,
+        boundaries: 10,
+        economy: 0,
+        society: 0,
+        ecosystem: 0,
+        rights: 0,
+        strategy: 0
+    },
+    {
+        question: "Les frontières doivent à terme être supprimées.",
+        answer: 0,
+        cause: 0,
+        boundaries: -10,
+        economy: 0,
+        society: 0,
+        ecosystem: 0,
+        rights: 0,
+        strategy: 0
+    },
+    {
+        question: "Il faut défendre ses idéaux, même si cela revient à trahir son pays.",
+        answer: 0,
+        cause: 0,
+        boundaries: -10,
+        economy: 0,
+        society: 0,
+        ecosystem: 0,
+        rights: 0,
+        strategy: 0
+    },
+    {
+        question: "Il est légitime pour un pays de défendre ses intérêts chez les autres pays en intervenant militairement.",
+        answer: 0,
+        cause: 0,
+        boundaries: 10,
+        economy: 0,
+        society: 0,
+        ecosystem: 0,
+        rights: 0,
+        strategy: 0
+    },
+    {
+        question: "Mon pays doit payer pour ses crimes passés.",
+        answer: 0,
+        cause: 0,
+        boundaries: -10,
+        economy: 0,
+        society: 0,
+        ecosystem: 0,
+        rights: 0,
+        strategy: 0
+    },
+    {
+        question: "Si les conditions et les normes sont compatibles, il faut supprimer les taxes pour les produits étrangers.",
+        answer: 0,
+        cause: 0,
+        boundaries: -10,
+        economy: 0,
+        society: 0,
+        ecosystem: 0,
+        rights: 0,
+        strategy: 0
+    },
+    
+    /* ECONOMIE ***************************************************** */
+    
     {
         question: "Les moyens de production ne devraient pas pouvoir être des propriétés privées.",
         answer: 0,
@@ -44,22 +304,11 @@ questions = [
         strategy: 0
     },
     {
-        question: "C'est l'offre et la demande qui permet de définir le prix d'un bien.",
+        question: "Le prix d'un bien ne devrait être déterminé que par l'offre et la demande.",
         answer: 0,
         cause: 0,
         boundaries: 0,
         economy: 10,
-        society: 0,
-        ecosystem: 0,
-        rights: 0,
-        strategy: 0
-    },
-    {
-        question: "Idéalement, la monnaie devrait être abolit.",
-        answer: 0,
-        cause: 0,
-        boundaries: 0,
-        economy: -10,
         society: 0,
         ecosystem: 0,
         rights: 0,
@@ -88,7 +337,7 @@ questions = [
         strategy: 0
     },
     {
-        question: "Les écoles et les universités peuvent être privée.",
+        question: "Il n'y a pas de problème à ce que les écoles et les universités puissent être privée.",
         answer: 0,
         cause: 0,
         boundaries: 0,
@@ -153,4 +402,6 @@ questions = [
         rights: 0,
         strategy: 0
     }
+    
+    /* SOCIETE ****************************************************** */
 ];
