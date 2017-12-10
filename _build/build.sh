434717dd1742ce9cc2737042b2dd5d575d504492
ManoIzquierda <manoizquierda.dev@gmail.com>
2017-12-10 19:53:57 +0100

    New translation system

---
+++ _build/build.sh
@@ -0,0 +1,29 @@
+#/bin/sh
+
+python3 _build/generate_config.py > _config.yml
+
+for l in $(cat _build/languages.txt)
+do
+	mkdir -p _$l
+	
+	echo "---" > _$l/index.html
+	echo "layout: index" >> _$l/index.html
+	echo "permalink: /"$l"/" >> _$l/index.html
+	echo "language: "$l >> _$l/index.html
+	echo "---" >> _$l/index.html
+	
+	echo "---" > _$l/help.html
+	echo "layout: help" >> _$l/help.html
+	echo "language: "$l >> _$l/help.html
+	echo "---" >> _$l/help.html
+	
+	echo "---" > _$l/quiz.html
+	echo "layout: quiz" >> _$l/quiz.html
+	echo "language: "$l >> _$l/quiz.html
+	echo "---" >> _$l/quiz.html
+	
+	echo "---" > _$l/results.html
+	echo "layout: results" >> _$l/results.html
+	echo "language: "$l >> _$l/results.html
+	echo "---" >> _$l/results.html
+done
