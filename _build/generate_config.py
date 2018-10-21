b888c6ede845e410622708996d23ef48571fd514
Gouvernathor <aanonyme.personne@hotmail.fr>
2018-10-21 17:35:32 +0200

    passage à la version simple

--- _build/generate_config.py
+++ _build/generate_config.py
@@ -1,35 +1,31 @@
 import sys
 import json
 
-configFn = "_build/_config_tpl.yml"
-languagesFn = "_build/languages.csv"
+filename = "_build/_config_tpl.yml"
 
-languages = {}
-with open(languagesFn, 'r') as f:
-	for line in f:
-		(lcode, lname, ldir) = line.split(";")
-		languages[lcode] = { "name":lname, "dir":ldir }
+languages = {
+	"en_US": "English",
+	"fr_FR": "Français"
+}
 
-with open(configFn, 'r') as f:
+with open(filename, 'r') as f:
 	current_line = None
 	counter = 0
 	line_buffer = []
 	for line in f:
 		if line.find("{{BUILD.TRANSLATIONS}}") >= 0:
 			print("t:")
-			for lcode, ldata in languages.items():
+			for lcode, lname in languages.items():
 				print("   "+lcode+":")
 				with open("_build/translation_"+lcode+".json") as translationFile:
-					print("      wdir: "+ldata["dir"])
-					
 					data = json.load(translationFile)
 					for tcode, tstring in data.items():
 						print("      "+tcode+": \""+tstring.replace("\"", "\\\"")+"\"")
 		elif line.find("{{BUILD.LANGUAGES}}") >= 0:
-			for lcode, ldata in languages.items():
+			for lcode, lname in languages.items():
 				print("   "+lcode+":")
 				print("      permalink: "+lcode+"/:name/")
-				print("      title: "+ldata["name"])
+				print("      title: "+lname)
 				print("      output: true")
 		else:
 			print(line.replace("\n", ""))
